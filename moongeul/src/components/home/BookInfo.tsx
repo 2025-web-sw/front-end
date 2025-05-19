8bc7eb7fc019eb6704358c4697c4adbc1f78cea4
유림 <yulimh001@gmail.com>
2025-05-20 06:05:26 +0900

    fix: bookInfo 컴포넌트 책 사진 w값 에러 조정

--- moongeul/src/components/home/BookInfo.tsx
+++ moongeul/src/components/home/BookInfo.tsx
@@ -8,7 +8,7 @@ const BookInfo = (props: Props) => {
   const { bookImage, title, author } = props
   return (
     <div className="border-light-gray flex items-center justify-start gap-x-2 rounded-[0.75rem] border p-2">
-      <div className="flex h-[3.5rem] w-[3] items-center justify-center overflow-hidden">
+      <div className="flex h-[3.5rem] w-[3.5rem] items-center justify-center overflow-hidden">
         <img src={bookImage} className="h-full w-full object-cover" alt="책 이미지" />
       </div>
       <section className="flex flex-col items-start justify-start">
